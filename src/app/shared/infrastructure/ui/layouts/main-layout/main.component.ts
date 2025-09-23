94f437fc6cc5cb336885dfa9be2636298b1ab99b
matthewsrt29 <matthew.tr29@gmail.com>
2025-09-22 23:52:40 -0500

    feat:header

---
+++ src/app/shared/infrastructure/ui/layouts/main-layout/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent} from '../../components/header/header.component';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [HeaderComponent],
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainLayoutComponent {
+
+}
