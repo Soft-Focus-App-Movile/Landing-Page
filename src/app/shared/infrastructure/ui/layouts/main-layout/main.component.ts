86ff720f16f8a26fdefce6df886ec2b08aee05f3
Ly009463 <maylhyolinda.gutierrez.15@gmail.com>
2025-09-28 11:17:56 -0500

    Revert "Merge branch 'feature/funcionalidades' into develop"
    This reverts commit c2e3617a989c3b363a91d3c3fd30ebd617e68ba9, reversing
    changes made to abf0e5f4d8be0eef16090709f1f72857778a5f60.

--- src/app/shared/infrastructure/ui/layouts/main-layout/main.component.ts
+++ src/app/shared/infrastructure/ui/layouts/main-layout/main.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent} from '../../components/header/header.component';
 import {HeroSectionComponent} from '../../../../../features/landing/infrastructure/ui/components/hero-section/hero-section.component';
-import {PlansSectionComponent} from '../../../../../features/landing/infrastructure/ui/components/plans-section/plans-section.component';
-import {AboutSectionComponent} from '../../../../../features/landing/infrastructure/ui/components/about-section/about-section.component';
-import {FeaturesSectionComponent} from '../../../../../features/landing/infrastructure/ui/components/features-section/features-section.component';
+import {
+  PlansSectionComponent
+} from '../../../../../features/landing/infrastructure/ui/components/plans-section/plans-section.component';
+import {
+  AboutSectionComponent
+} from '../../../../../features/landing/infrastructure/ui/components/about-section/about-section.component';
 import {FooterComponent} from '../../components/footer/footer.component';
-import {HeaderComponent} from '../../components/header/header.component';
 
 @Component({
   selector: 'app-main-layout',
@@ -13,8 +16,7 @@ import {HeaderComponent} from '../../components/header/header.component';
     HeroSectionComponent,
     PlansSectionComponent,
     AboutSectionComponent,
-    FooterComponent,
-    FeaturesSectionComponent
+    FooterComponent
   ],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
