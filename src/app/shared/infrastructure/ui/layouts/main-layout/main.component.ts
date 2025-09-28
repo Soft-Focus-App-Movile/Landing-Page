d3350d7056f98d9eca5ad4f56511ad59e2664ec3
Giancarlo Solis <u202318615@upc.edu.pe>
2025-09-27 19:08:45 -0500

    feat:Adding the plans section

--- src/app/shared/infrastructure/ui/layouts/main-layout/main.component.ts
+++ src/app/shared/infrastructure/ui/layouts/main-layout/main.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent} from '../../components/header/header.component';
 import {HeroSectionComponent} from '../../../../../features/landing/infrastructure/ui/components/hero-section/hero-section.component';
+import {
+  PlansSectionComponent
+} from '../../../../../features/landing/infrastructure/ui/components/plans-section/plans-section.component';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [HeaderComponent, HeroSectionComponent],
+  imports: [HeaderComponent, HeroSectionComponent, PlansSectionComponent],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
